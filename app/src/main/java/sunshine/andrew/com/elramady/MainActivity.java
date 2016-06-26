3ae52207a202596370ca1ad7051351c9ef6f0edc
endo1monir <andrew.monir@yahoo.com>
2016-06-26 14:40:26 +0200

    Initial commit

---
+++ app/src/main/java/sunshine/andrew/com/elramady/MainActivity.java
@@ -0,0 +1,62 @@
+package sunshine.andrew.com.elramady;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement action_worker action_traders
+        if (id == R.id.action_client) {
+startActivity(new Intent(MainActivity.this,addClient.class));
+            return true;
+        }
+        if (id == R.id.action_worker) {
+            startActivity(new Intent(MainActivity.this,addWorker.class));
+            return true;
+        }
+        if (id == R.id.action_traders) {
+            startActivity(new Intent(MainActivity.this,addTrader.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/sunshine/andrew/com/elramady/addWorker.java
@@ -0,0 +1,13 @@
+package sunshine.andrew.com.elramady;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addWorker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_worker);
+    }
+}
